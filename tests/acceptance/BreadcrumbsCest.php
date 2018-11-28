fe11332530cc95961b62d8aa9372d4ba5f052e9a
overclokk <info@overclokk.net>
2018-11-28 10:07:08 +0100

    Added some acceptance tests

--- tests/acceptance/BreadcrumbsCest.php
+++ tests/acceptance/BreadcrumbsCest.php
@@ -5,37 +5,44 @@ class BreadcrumbsCest
 {
     public function _before(AcceptanceTester $I)
     {
-        // $I->loginAsAdmin();
-        // $I->amOnPluginsPage();
-        // $I->seePluginInstalled( 'breadcrumbs' );
-        // $I->seePluginActivated( 'breadcrumbs/index.php' );
+        $I->loginAsAdmin();
+        $I->amOnPluginsPage();
+        $I->seePluginInstalled( 'breadcrumbs' );
+        $I->seePluginActivated( 'breadcrumbs' );
     }
 
     public function _after(AcceptanceTester $I)
     {
     }
 
-    // tests
-    // public function tryToTest(AcceptanceTester $I)
-    // {
-    //     $content = implode( ' ', array_fill( 0, 550, 'lorem' ) );
-    //     $page_id = $I->havePageInDatabase( [
-    //         'post_title'   => 'Home',
-    //         'post_content' => $content,
-    //     ] );
-    //     $I->amOnPage( "/?p={$page_id}" );
-    //     $I->see( "Home" );
-    // }
-
-    // tests
-    public function tryToTest2(AcceptanceTester $I)
+    /**
+     * @test
+     * it should render breadcrumbs
+     */
+    public function it_should_render_breadcrumbs(AcceptanceTester $I)
     {
-        // $content = implode( ' ', array_fill( 0, 550, 'lorem' ) );
-        // $page_id = $I->havePageInDatabase( [
-        //     'post_title'   => 'Home',
-        //     'post_content' => $content,
-        // ] );
-        // $I->amOnPage( '/' );
-        // $I->seeElement( 'breadcrumb' );
+        $content = implode( ' ', array_fill( 0, 550, 'lorem' ) );
+        $page_id = $I->havePageInDatabase( [
+            'post_title'   => 'Home',
+            'post_content' => $content,
+        ] );
+        $I->amOnPage( '/' );
+        $I->seeElement( 'nav', ['aria-label' => 'breadcrumb'] );
+        $I->seeElement( 'ol', ['itemtype' => 'https://schema.org/BreadcrumbList'] );
+        // $I->seeElement( 'a', ['itemprop' => 'item', 'href' => get_home_url( null, '/' ) ] );
+        // $I->seeElement( 'a', ['itemprop' => 'item', 'href' => 'http://moduli.test/' ] );
+    }
+
+    /**
+     * @test
+     * it should render breadcrumbs_on_page
+     */
+    public function it_should_render_breadcrumbs_on_page(AcceptanceTester $I)
+    {
+        $page_id = $I->havePageInDatabase( [
+            'post_title'   => 'Page',
+        ] );
+        $I->amOnPage( "/?p={$page_id}" );
+        $I->seeElement( '.breadcrumb' );
     }
 }
